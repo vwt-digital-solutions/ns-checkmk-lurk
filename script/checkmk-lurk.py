485bda21e362295c53dc8078855a59cbacbe5317
JelleBwns <jellebouwens@hotmail.com>
2020-05-18 10:23:18 +0200

    DAT-2965 Add function to get Oauth access token

--- script/checkmk-lurk.py
+++ script/checkmk-lurk.py
@@ -1,3 +1,17 @@
 import config
 
-# config.SERVERS
+import requests
+
+
+def get_oath_token():
+    data = {
+        "client_id": config.OAUTH_CLIENT_ID,
+        "client_secret": config.OAUTH_CLIENT_SECRET,
+        "scope": config.OAUTH_CLIENT_SCOPE,
+        "grant_type": "client_credentials"
+    }
+    request = requests.post(config.OAUTH_TOKEN_URL,
+                            data=data).json()
+
+    return request["access_token"]
+
