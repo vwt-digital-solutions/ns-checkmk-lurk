563c682a13bbfa9fd9b280d93b3893e34d1708eb
JelleBwns <jellebouwens@hotmail.com>
2020-05-18 10:27:08 +0200

    DAT-2965 Add config back

---
+++ script/config.py
@@ -0,0 +1,13 @@
+# Uncomment with correct values
+#
+# OAUTH_CLIENT_ID = ""
+# OAUTH_CLIENT_SECRET = ""
+# OAUTH_CLIENT_SCOPE = ""
+# OAUTH_TOKEN_URL = ""
+#
+# API_URL = ""
+#
+# SERVERS = [
+#     "<sitepath>/tmp/run/live",
+#     ("localhost", 6557)
+# ]
