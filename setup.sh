586b69043411645f2682026a8ed4fe4420c67ae5
DionWezz <dionwesteneng@hotmail.com>
2020-06-02 16:51:41 +0200

    DAT-3140 Added the check for the crontabs

--- setup.sh
+++ setup.sh
@@ -6,6 +6,42 @@ function no_pip {
   exit	
 }
 
+# function for installing the event crontab
+function install_event_cron {
+  echo "event crontab not yet installed ..."
+
+  # write new crontab file
+  crontab -l > checkmk-lurk-event
+
+  #write entry's to file
+  echo "# Crontab for retrieving event data every minute" >> checkmk-lurk-event
+  echo "* * * * * /usr/bin/env python3 $(pwd)/script/checkmk-lurk.py --data event" >> checkmk-lurk-event
+
+  # install new cron
+  crontab checkmk-lurk-event
+  rm checkmk-lurk-event
+
+  echo "event crontab installed!"
+}
+
+# function for installing the performance crontab
+function install_perf_cron {
+  echo "performance crontab not yet installed ..."
+
+  # write new crontab file
+  crontab -l > checkmk-lurk-perf
+
+  #write entry's to file
+  echo "# Crontab for retrieving performance data every 5 minutes" >> checkmk-lurk-perf
+  echo "* * * * * /usr/bin/env python3 $(pwd)/script/checkmk-lurk.py --data performance" >> checkmk-lurk-perf
+
+  # install new cron
+  crontab checkmk-lurk-perf
+  rm checkmk-lurk-perf
+
+  echo "performance crontab installed!"
+}
+
 # check if python3-pip is installed
 # if not, install it
 echo "checking if python3-pip is installed"
@@ -15,18 +51,11 @@ dpkg -l | grep -qw python3-pip || no_pip
 echo "installing the python requirements ..."
 python3 -m pip install -r requirements.txt
 
-echo "installing crontabs ..."
-# Write out current crontab
-crontab -l > checkmk-lurk
+echo "Checking if crontabs are already installed ..."
 
-# echo new cron lines into cron file
-echo "# Crontab for retrieving performance data every 5 minutes" >> checkmk-lurk
-echo "*/5 * * * * /usr/bin/env python3 $(pwd)/script/checkmk-lurk.py --data performance" >> checkmk-lurk
-echo "# Crontab for retrieving event data every minute" >> checkmk-lurk
-echo "* * * * * /usr/bin/env python3 $(pwd)/script/checkmk-lurk.py --data event" >> checkmk-lurk
-
-# install new cron
-crontab checkmk-lurk
-rm checkmk-lurk
+# check for performance data crontab
+crontab -l -u dwesteneng | grep $(pwd)'/script/checkmk-lurk.py --data performance' && echo "performance crontab already installed!" || install_perf_cron
+# check for event data crontab
+crontab -l -u dwesteneng | grep $(pwd)'/script/checkmk-lurk.py --data event' && echo "event crontab already installed!" || install_event_cron
 
 echo "setup completed!"
